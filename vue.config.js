7f10e5a0bf6a6adf8b886cf15006c514f9c79fab
1056438319 <Feng0808>
2022-09-16 17:24:05 +0800

    更新端口

--- vue.config.js
+++ vue.config.js
@@ -9,7 +9,7 @@ const CompressionPlugin = require('compression-webpack-plugin')
 
 const name = process.env.VUE_APP_TITLE || '小红书博主管理' // 网页标题
 
-const port = process.env.port || process.env.npm_config_port || 1023 // 端口
+const port = process.env.port || process.env.npm_config_port || 1779 // 端口
 
 // vue.config.js 配置说明
 //官方vue.config.js 参考文档 https://cli.vuejs.org/zh/config/#css-loaderoptions
@@ -35,7 +35,7 @@ module.exports = {
     proxy: {
       // detail: https://cli.vuejs.org/config/#devserver-proxy
       [process.env.VUE_APP_BASE_API]: {
-        target: `http://localhost:4399`,
+        target: `http://red.miemie.cc:4399`,
         changeOrigin: true,
         pathRewrite: {
           ['^' + process.env.VUE_APP_BASE_API]: ''
